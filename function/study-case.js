4a01702671b5cbc722511c09e16209f3abc607e0
Abiyansyahh <bibibee027@gmail.com>
2023-12-19 11:39:04 +0700

    feat: Function

---
+++ function/study-case.js
@@ -0,0 +1,10 @@
+function greetingProgram(nama) {
+    return "hallo selamat datang," + name + ", di CV ZAMAN NOW";
+}
+console.log(greetingProgram(abi));
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+console.log(add(10, 5));
