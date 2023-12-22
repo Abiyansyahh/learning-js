daa399efccf7ad7fb11dd08c283eb701d6ac35d0
Abiyansyahh <bibibee027@gmail.com>
2023-12-22 14:39:01 +0700

    feat: add array method

---
+++ array-methods/index.js
@@ -0,0 +1,25 @@
+//function bisa disebut methods
+
+const fruits = ['Apple','Mango','Grape','Watermelon'];
+
+//array push
+fruits.push('sursop');
+
+//console.log(fruit)
+console.log(fruits.toString())
+
+//latihan
+const person = ['budi','joko','wahyu']
+
+person.push('william','erik','thomas','bibi')
+console.log(person.toString())
+
+person.forEach(function(data){
+    console.log(data)
+})
+
+// tugas harian
+//buat 5 array yang kemudian akan ditambahkan data method push()
+//setelah data array ditambahkan keluarkan data dan tampilkan dengan forEach
+//5 arrat dengan nama my hobbies, myfriends, myfavouriteFoods, myLanguage
+//file study-case.js
