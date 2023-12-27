67cb6099a777d3344153c91446e5448df312e296
Abiyansyahh <bibibee027@gmail.com>
2023-12-27 17:57:57 +0700

    feat: add study-case-arrow

---
+++ arrow-function/study-case.js
@@ -0,0 +1,11 @@
+const Pertambahan = (num1, num2) => num1 + num2;
+console.log(Pertambahan(6, 5));
+
+const Pengurangan = (num1, num2) => num1 - num2;
+console.log(Pengurangan(7, 3));
+
+const Pembagian = (num1, num2) => num1 / num2;
+console.log(Pembagian(28, 4));
+
+const Perkalian = (num1, num2) => num1 * num2;
+console.log(Perkalian(8, 4));
