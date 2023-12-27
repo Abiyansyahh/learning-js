71462e11baaccecb5308291da27126469888e9b4
Abiyansyahh <bibibee027@gmail.com>
2023-12-27 14:17:22 +0700

    feat: add asynchronous

---
+++ basic-async/example.js
@@ -0,0 +1,28 @@
+function taburkanBijiKopi() {
+    setTimeout(function() {
+        console.log("Menuangkan biji kopi...")
+    })
+}
+
+function panaskanAir() {
+    setTimeout(function() {
+        console.log("Sedang memanaskan air...")
+    })
+}
+
+function siap() {
+    setTimeout(function() {
+        console.log("Kopi siap dinikmati!")
+    })
+}
+
+function adukKopi() {
+    setTimeout(function() {
+        console.log("Mengaduk kopi...")
+    })
+}
+
+taburkanBijiKopi()
+panaskanAir()
+siap()
+adukKopi()
